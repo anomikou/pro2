c2b3e0abda2853895964ea898cc7b524392245f3
Panagiota Angelopoulou <36485842+Nayaangelopoulou@users.noreply.github.com>
2018-11-11 14:32:43 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Main {
 
@@ -6,5 +8,28 @@ public class Main {
 
 		Scanner input = new Scanner(System.in);
 
+		System.out.println("Please enter all the characteristics of the attributes. When finsh press 0");
+
+		ArrayList<String> titles = new ArrayList<String>();
+
+		String t = null;
+
+		while(true) {
+
+			t = input.nextLine();
+
+			if(t.equals("stop")) {
+
+				break;
+
+			} else {
+
+				titles.add(t);
+
+			}
+
+		}
+
+		System.out.println("All the characteristics are saved.");
 	}
 }
