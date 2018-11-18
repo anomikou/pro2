6b95accb36dcaf732724409f24f5bf141321f37e
anomikou <44942158+anomikou@users.noreply.github.com>
2018-11-18 16:17:29 +0200

    Add files via upload

---
+++ Att1.java
@@ -0,0 +1,25 @@
+public class Att1 {
+
+	String natt;
+	String vatt;
+
+	public void setNatt( String n ) {
+		natt = n;
+	}
+	
+	public void setVatt( String v ) {
+		vatt=v;
+	}
+
+	public String getNatt() {
+		return natt;
+	}
+
+	public String getVatt() {
+		return vatt;
+	}
+
+
+
+
+}
